7718365a9e772e4ea68794afe4affef25c8b45b7
ludeboss <ludeboss1812@dobosz.co.uk>
2018-12-21 22:20:49 +0000

    18 lekcja

---
+++ Menu.java
@@ -0,0 +1,23 @@
+
+package com.tnenewboston.travis;
+
+import android.app.List.Activity;
+
+public class Menu extends ListAvtivity {
+
+  String classes[] = {"startingPoint", "lukasz1", "lukasz2"};
+                       |
+                       musi byc jak activity
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(1, v, position, id);
+  }
+
+  @Override
+  protection void onCreate(Bundle savedInstanceState
+    super.onCreate
+  }
+
+}
+
