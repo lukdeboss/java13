e849996908b5c8ab01dbbc54cfbd11211f80f2fb
ludeboss <ludeboss1812@dobosz.co.uk>
2018-12-21 22:22:07 +0000

    20 lekcja

--- Menu.java
+++ Menu.java
@@ -2,22 +2,31 @@
 package com.tnenewboston.travis;
 
 import android.app.List.Activity;
+import ... arrayAdapter
 
 public class Menu extends ListAvtivity {
 
   String classes[] = {"startingPoint", "lukasz1", "lukasz2"};
-                       |
-                       musi byc jak activity
-
-  @Override
-  protected void onListItemClick(ListView l, View v, int position, long id) {
-    super.onListItemClick(1, v, position, id);
-  }
 
   @Override
   protection void onCreate(Bundle savedInstanceState
-    super.onCreate
+    super.onCreate(savedInstanceState);
+    setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1 , classes));
   }
+ 
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(1, v, position, id);
+    Class ourClass = Class.forName("com.thenewboston.travis.startingPoint");
+                                    |
+                                    to jest referencja do pliku startingPoint.java
+    Intent ourIntent = new Intent(Menu.this, ourClass);
+                                   |         |
+                                   |         nazwa klasy
+                                   to jest context, czyli ze w klasie Menu
+    startActivity(ourIntent);
+  }
+
 
 }
 
